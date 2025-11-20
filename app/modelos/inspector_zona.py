3b7964eb179da9ec375c6ac561b8b64856589124
Gilson123tenemea <tenemeaaguilar@gmail.com>
2025-11-20 07:42:28 -0500

    Estructura de validaciones

--- app/modelos/inspector_zona.py
+++ app/modelos/inspector_zona.py
@@ -1,12 +1,12 @@
 # app/modelos/inspector_zona.py
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date, func
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, DateTime, func
 from app.config import Base
 
 class InspectorZona(Base):
     __tablename__ = "inspector_zona"
 
     id_inspector_zona = Column(Integer, primary_key=True, index=True)
-    fecha_asignacion = Column(Date, nullable=False, server_default=func.current_date())
+    fecha_asignacion = Column(DateTime, nullable=False, server_default=func.now())
     borrado = Column(Boolean, default=True)
 
     id_inspector_inspectorzona = Column(Integer, ForeignKey("inspector.id_inspector"), nullable=False)
