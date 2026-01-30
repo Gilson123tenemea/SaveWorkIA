4aa1383117e928142018a683b277b35c6b69334b
Gilson123tenemea <tenemeaaguilar@gmail.com>
2026-01-30 05:28:38 -0500

    Update CORS for Azure frontend

--- app/main.py
+++ app/main.py
@@ -97,18 +97,20 @@ app = FastAPI(
 # 🔹 Configuración de CORS (para permitir peticiones desde el frontend)
 # ----------------------------------------------------------------------
 origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:5173"
+    "http://localhost:3000",       
+    "http://127.0.0.1:3000",       
+    "http://127.0.0.1:5173",         
+    "http://104.45.177.193:3000",    
+    "https://tudominio.com",         
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,      # puedes usar ["*"] si aún no tienes dominio
+    allow_origins=origins,    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # ----------------------------------------------------------------------
 # 🔹 Registrar Routers
 # ----------------------------------------------------------------------
